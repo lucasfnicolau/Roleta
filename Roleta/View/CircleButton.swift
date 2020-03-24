7fea5971189142c18529b3b9adbb02a6f2bdb5d5
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-24 20:19:12 -0300

    App finished and ready to go to the AppStore :D

--- Roleta/View/CircleButton.swift
+++ Roleta/View/CircleButton.swift
@@ -16,4 +16,35 @@ class CircleButton: UIButton {
         self.layer.cornerRadius = self.frame.width / 2
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 0.6
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 0.6
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 1
+        }
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 1
+        }
+    }
 }
